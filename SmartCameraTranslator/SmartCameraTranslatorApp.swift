1533faa7068819dcf094a9a6a9faf772576c465a
Nazrin Atayeva <92153363+neomond@users.noreply.github.com>
2025-08-17 22:33:07 +0400

    Initial Commit

---
+++ SmartCameraTranslator/SmartCameraTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCameraTranslatorApp.swift
+//  SmartCameraTranslator
+//
+//  Created by Nazrin Atayeva on 17.08.25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCameraTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
